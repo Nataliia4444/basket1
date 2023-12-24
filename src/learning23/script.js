1f60d4c235066e1c48fbb3319f6a98295e3bfef0
Nataliia4444 <127755653+Nataliia4444@users.noreply.github.com>
2023-12-24 10:53:09 +0200

    add change

--- src/learning23/script.js
+++ src/learning23/script.js
@@ -2,15 +2,16 @@ const refs = {
   option: document.querySelectorAll('.option'),
   title: document.querySelector('.title'),
   select: document.getElementById('standard-select'),
-  button: document.querySelector('.btn'),
 };
 console.log(refs.option);
-// refs.option.forEach(el => {
-//   el.addEventlistener('select', function (e) {
-//     console.log(e);
-//   });
-// });
-
-refs.button.addEventlistener('click', function () {
-  console.log('agsge');
+refs.option.forEach(el => {
+  console.log(el);
+  el.addEventlistener('click', function (e) {
+    console.log(e);
+  });
 });
+
+// const btn = document.querySelector('.btn');
+// btn.addEventListener('click', function () {
+//   console.log(refs.select.value);
+// });
